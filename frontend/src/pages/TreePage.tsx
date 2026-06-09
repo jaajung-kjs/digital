89e4b81ec433a355b339f69091cf8a7de6085b68
jaajung-kjs <jaajung@naver.com>
2026-06-09 14:08:27 +0900

    feat(ui): 보호 라우트를 AppShell 아래 중첩 + 홈 좌 트리 제거(셸로 이동)

--- frontend/src/pages/TreePage.tsx
+++ frontend/src/pages/TreePage.tsx
@@ -1,13 +1,9 @@
-import { TreePanel } from '../components/tree/TreePanel';
 import { TreeVisualization } from '../components/tree/TreeVisualization';
 import { StatsSidePanel } from '../components/tree/StatsSidePanel';
 
 export function TreePage() {
   return (
     <div className="flex h-full overflow-hidden">
-      <aside className="w-72 border-r bg-white overflow-y-auto flex-shrink-0">
-        <TreePanel />
-      </aside>
       <main className="flex-1 overflow-auto bg-gray-50">
         <TreeVisualization />
       </main>
